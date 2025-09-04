86efd5a35488c3c794337320a5b5dc3cc45a1cc9
Clayton <clay10bg@gmail.com>
2025-09-04 16:19:21 -0600

    Fix Display and Move Back and Clear Buttons Right
    Fixed some dispaly issues, making the display wider after finding that
    numbers will automatically convert to scientific notation after 20
    characters long. Also move the 'back' and 'clear' buttons over to the
    right to make the calculator look nicer with a wide landscape
    orientation. Decimal point can now only be added once. Fixed an issue
    where calculator was returning NaN because the Clear button was not
    setting the operator to "" (nothing).
    Required functionality for project has been achieved with the added
    bonus of decimals and backspace buttons.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function add (a, b) {
-    return (a + b).toFixed(valueA.length);
+    return a + b;
 }
 
 function subtract (a, b) {
@@ -11,7 +11,8 @@ function multiply (a, b) {
 }
 
 function divide (a, b) {
-    return a / b;
+    if (b == 0) return "Cannot Divide by Zero :P";
+    else return a / b;
     // if (a % b == 0) return a / b;
     // else {
     //     let decimal = 0;
@@ -31,8 +32,7 @@ function operate (a, b, c) {
             return multiply(Number(a), Number(b));
             break;
         case "/":
-            if (b == 0) return "Div by Zero";
-            else return divide(Number(a), Number(b));
+            return divide(Number(a), Number(b));
             break;
     }
 }
@@ -40,7 +40,7 @@ function operate (a, b, c) {
 let valueA = "N"; //previous value
 let valueB = "N"; //current value
 let operator = "";
-let equals = false; //set to true whenpressing the equals button and false when pressing any other operator
+let equals = true; //set to true whenpressing the equals button and false when pressing any other operator
 
 const display = document.querySelector(".display");
 const operators = document.querySelectorAll(".operator");
@@ -57,10 +57,15 @@ buttons.forEach((button) => {
                 display.textContent = 0;
                 valueA = "N";
                 valueB = "N";
+                operator = "";
                 break;
             case "Back":
-                if (display.textContent.length == 1) display.textContent = 0;
-                else display.textContent = display.textContent.slice(0, display.textContent.length-1);
+                if (!equals) {
+                    if (display.textContent.length == 1) display.textContent = 0;
+                    else display.textContent = display.textContent.slice(0, display.textContent.length-1);
+                    if (operator == "") valueA = display.textContent;
+                    else valueB = display.textContent;
+                }
                 break;
             case "+":
             case "-":
@@ -79,37 +84,70 @@ buttons.forEach((button) => {
                 valueB = 0;
                 break;
             case "=":
-                display.textContent = operate(valueA, valueB, operator);
-                // button.style.backgroundColor = "green";
-                valueA = display.textContent;
-                equals = true;
+                if (operator != "") { //requires some operator to have been pressed previously
+                    console.log(operator);
+                    display.textContent = operate(valueA, valueB, operator);
+                    // button.style.backgroundColor = "green";
+                    valueA = display.textContent;
+                    equals = true;
+                }
                 break;
-            default:
-                if (equals){
+            case ".":
+                if (!display.textContent.includes(".")) {
+                     if (equals){ //if last button was equals, start fresh
+                        valueA = "N";
+                        valueB = "N";
+                        operator = "";
+                    }
+                    if (valueB == "N") { //if B is not assigned yet, start with A
+                        if (valueA == "N" || valueA === 0) valueA = button.textContent;
+                        else if (valueA.length < 20) valueA += button.textContent;
+                        display.textContent = valueA;
+                    }
+                    else {
+                        if (valueB == "N" || valueB === 0) valueB = button.textContent;
+                        else if (valueB.length < 20) valueB += button.textContent;
+                        display.textContent = valueB;
+                    }
+                    equals = false;
+                }
+                break;
+            default: //numbers
+                if (equals){ //if last button was equals, start fresh
                     valueA = "N";
                     valueB = "N";
+                    operator = "";
                 }
                 if (valueB == "N") { //if B is not assigned yet, start with A
                     if (valueA == "N" || valueA === 0) valueA = button.textContent;
-                    else valueA += button.textContent;
+                    else if (valueA.length < 20) valueA += button.textContent;
                     display.textContent = valueA;
                 }
                 else {
                     if (valueB == "N" || valueB === 0) valueB = button.textContent;
-                    else valueB += button.textContent;
+                    else if (valueB.length < 20) valueB += button.textContent;
                     display.textContent = valueB;
                 }
                 equals = false;
             break;
         } //end switch button text content
-        console.log("Display Length: "+display.textContent.length);
-        if (display.textContent.length > 10) {
-            if (display.textContent.includes(".")) {
-                if (display.textContent.length > 11) {
-                    display.textContent = display.textContent.slice(0, 11);
-                }
-            }
-            else display.textContent = display.textContent.slice(0, 10);
-        }
+        // console.log("Display Length: "+display.textContent.length);
+        console.log(display.textContent);
+        // if (display.textContent.length > 10) {
+            // if (display.textContent.includes(".")) {
+            //     if (display.textContent.length > 11) {
+            //         display.textContent = display.textContent.slice(0, 11);
+            //     }
+                // Remove trailing zeros
+            //     if (display.textContent != 0) {
+            //         let len = display.textContent.length-1;
+            //         for (let i = len; i > 0 && display.textContent[len] == 0; i++) {
+            //             display.textContent = display.textContent.slice(0, len);
+            //             len--;
+            //         }
+            //     }
+            // }
+            // else display.textContent = display.textContent.slice(0, 10);
+        // }
     })
 })
